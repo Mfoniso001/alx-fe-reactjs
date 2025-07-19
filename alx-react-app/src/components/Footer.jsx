e0eff1ea2954f8bcf545b2a5fec1ccb36a690dd8
Mfoniso George <constancelestine16@gmail.com>
2025-07-19 17:49:08 +0100

    Initial commit for new inline style

---
+++ alx-react-app/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: '#333', color: '#ccc', textAlign: 'center', padding: '10px' }}>
+      <p>&copy; 2025 My Favorite Cities</p>
+    </footer>
+  );
+}
+
+export default Footer;
