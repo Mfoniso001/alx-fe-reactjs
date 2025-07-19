00120b780079192e4f739f375b19b7e814d3735c
Mfoniso George <constancelestine16@gmail.com>
2025-07-19 18:02:54 +0100

    Maincontent styling

--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -3,7 +3,7 @@
 function MainContent() {
   return (
     <main style={{ padding: '20px', backgroundColor: '#e8f0fe' }}>
-      <p>Welcome to my React app showcasing some of my favorite cities!</p>
+      <p>"I love to visit New York, Paris, and Tokyo."</p>
     </main>
   );
 }
