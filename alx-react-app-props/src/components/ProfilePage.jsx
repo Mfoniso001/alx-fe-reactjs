1b6b5fdcb7491de20635ab26e4cce6ef1da64926
Mfoniso George <constancelestine16@gmail.com>
2025-07-24 03:01:15 +0100

    Prop drilling

---
+++ alx-react-app-props/src/components/ProfilePage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import UserInfo from "./UserInfo";
+
+function ProfilePage() {
+  return (
+    <div>
+      <h1>Welcome!</h1>
+      <UserInfo />
+    </div>
+  );
+}
+
+export default ProfilePage;
+
+
