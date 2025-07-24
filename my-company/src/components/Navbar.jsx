80333a1b8c17975f5b1005344c9ee1130163b052
Mfoniso George <constancelestine16@gmail.com>
2025-07-24 04:07:00 +0100

    Nav bar

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,14 +1,30 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Navbar() {
+  const navStyle = {
+    display: "flex",
+    justifyContent: "space-around",
+    alignItems: "center",
+    backgroundColor: "#333",
+    padding: "10px",
+    color: "white",
+  };
+
+  const linkStyle = {
+    color: "white",
+    textDecoration: "none",
+    fontWeight: "bold",
+  };
+
   return (
-    <nav style={{ padding: '10px', backgroundColor: '#f0f0f0' }}>
-      <Link to="/" style={{ margin: '0 10px' }}>Home</Link>
-      <Link to="/about" style={{ margin: '0 10px' }}>About</Link>
-      <Link to="/services" style={{ margin: '0 10px' }}>Services</Link>
-      <Link to="/contact" style={{ margin: '0 10px' }}>Contact</Link>
+    <nav style={navStyle}>
+      <Link to="/" style={linkStyle}>Home</Link>
+      <Link to="/about" style={linkStyle}>About</Link>
+      <Link to="/services" style={linkStyle}>Services</Link>
+      <Link to="/contact" style={linkStyle}>Contact</Link>
     </nav>
   );
 }
 
 export default Navbar;
+
