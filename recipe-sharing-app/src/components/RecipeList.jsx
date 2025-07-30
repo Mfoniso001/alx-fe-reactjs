5e459f1d27af80eefef9f6b343c1c94a90a41cd4
Mfoniso George <constancelestine16@gmail.com>
2025-07-30 06:35:41 +0100

    updated recipe-sharing app

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { useRecipeStore } from './recipeStore';
+import { Link } from 'react-router-dom';
 
 const RecipeList = () => {
   const filteredRecipes = useRecipeStore((state) => state.filteredRecipes);
 
   return (
     <div>
+      <h2>Recipes</h2>
+
+     
+      <Link to="/add" style={{ textDecoration: 'none', color: 'blue' }}>
+        + Add New Recipe
+      </Link>
+
       {filteredRecipes.length === 0 ? (
         <p>No matching recipes found.</p>
       ) : (
