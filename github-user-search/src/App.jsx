3cc9db179364dcee455f01879f443b9461baa204
Mfoniso George <constancelestine16@gmail.com>
2025-08-01 12:55:55 +0100

    Add Github user serach project setup

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>GitHub User Search</h1>
+      <p>Start typing to search for a GitHub user.</p>
+    </div>
+  );
+}
+
+export default App;
