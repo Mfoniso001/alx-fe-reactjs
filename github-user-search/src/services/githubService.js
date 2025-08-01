b23b7beb2fb1926078826f2efd2e7d199e3d2a51
Mfoniso George <constancelestine16@gmail.com>
2025-08-01 14:35:25 +0100

    added new files

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,11 +1,10 @@
+import axios from "axios";
+
 export const fetchUserData = async (username) => {
-    const response = await fetch(`https://api.github.com/users/${username}`);
-  
-    if (!response.ok) {
-      throw new Error("User not found");
-    }
-  
-    const data = await response.json();
-    return data;
-  };
-  
+  try {
+    const response = await axios.get(`https://api.github.com/users/${username}`);
+    return response.data;
+  } catch (error) {
+    throw new Error("User not found");
+  }
+};
