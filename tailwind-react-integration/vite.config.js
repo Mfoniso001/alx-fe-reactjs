3d4d3cbabb92ed031c316ca50720fde148a49cbc
Mfoniso George <constancelestine16@gmail.com>
2025-08-09 22:10:58 +0100

    TailwindCSS integration

--- tailwind-react-integration/vite.config.js
+++ tailwind-react-integration/vite.config.js
@@ -1,8 +1,13 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // ✅ Add this
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(), 
+  ],
 })
 
 
