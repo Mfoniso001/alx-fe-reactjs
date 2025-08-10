80792992eb16577a3e2ddd9d11854a19582cd154
Mfoniso George <constancelestine16@gmail.com>
2025-08-10 10:08:40 +0100

    Add components

--- tailwind-react-integration/vite.config.js
+++ tailwind-react-integration/vite.config.js
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+  ],
   css: {
-    transformer: 'postcss'
-  }
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
 })
+
 
 
 
